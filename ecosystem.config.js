48cd988fff01662ce8c23856d3b57b6c7f882595
Andrii Vynohradov <andrii.vynohradov@b4value.net>
2026-06-28 03:15:58 -0700

    refactor: better structure

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: 'resomd-server',
+      name: 'resomd-api',
       script: 'dist/src/main.js', // Path to the compiled NestJS server main file
-      cwd: './apps/server',
+      cwd: './apps/api',
       instances: 1,
       autorestart: true,
       max_restarts: 10,
       restart_delay: 2000,
     },
     {
-      name: 'resomd-web',
-      script: 'node_modules/vite/bin/vite.js', // Launch Vite executable directly
-      args: 'preview --host 0.0.0.0', // Preview built assets using Vite's preview settings
+      name: 'resomd',
+      script: 'dist/src/main.js',
       cwd: './apps/web',
       instances: 1,
       autorestart: true,
